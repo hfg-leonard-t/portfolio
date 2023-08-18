17007562df36edf737acc5723ff72ef87f35d37b
hfg-leonard-t <leonard.thillmann@hfg.design>
2023-08-18 15:20:44 +0200

    threeSection working

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,7 @@
 let reveal = document.querySelectorAll(".reveal");
 
 reveal.forEach((el) => {
-  let headings = el.querySelectorAll("div, p, span, img");
+  let headings = el.querySelectorAll("div, p, span, img, button");
   let btn = el.querySelector(".btn");
 
   let tl = gsap
@@ -23,4 +23,39 @@ reveal.forEach((el) => {
     toggleActions: "play none none reverse ",
     animation: tl,
   });
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+const images = document.querySelectorAll(".image-container img");
+
+images.forEach((image) => {
+  gsap.to(image, {
+    opacity: 1,
+    scrollTrigger: {
+      trigger: image,
+      start: "top center", // When the top of the image hits the center of the viewport
+      end: "bottom center", // When the bottom of the image hits the center of the viewport
+      scrub: true, // Smoothly animate opacity during scroll
+    },
+  });
+});
+
+const elements = document.querySelectorAll(".idle-animation");
+
+// Loop through each element and create animations
+elements.forEach((element, index) => {
+  gsap.fromTo(
+    element,
+    { opacity: 0, y: -20 },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 0.5,
+      delay: 0.5 + index * 0.3, // Delay each element by 0.5 seconds
+      scrollTrigger: {
+        trigger: element,
+      },
+    }
+  );
 });
---
+++ portfolio/threeSection.js
@@ -0,0 +1,178 @@
+gsap.registerPlugin(ScrollTrigger);
+let sceneInitialized = false;
+
+const scrollTriggerElement = document.getElementsByClassName("more-wrapper");
+
+ScrollTrigger.create({
+  trigger: scrollTriggerElement,
+  onEnter: () => {
+    initializeScene();
+  },
+});
+
+const initializeScene = () => {
+  if (!sceneInitialized) {
+    const { AmmoPhysics, PhysicsLoader } = ENABLE3D;
+    const modelPaths = [
+      "./assets/glb/aileronMount.glb",
+      "./assets/glb/bellShield.glb",
+      "./assets/glb/bikeBacklightMount.glb",
+      "./assets/glb/buttonInterface.glb",
+      "./assets/glb/chickenFlapSpool.glb",
+      "./assets/glb/clothingHook.glb",
+      "./assets/glb/headphoneHook.glb",
+      "./assets/glb/irMount.glb",
+      "./assets/glb/jet.glb",
+      "./assets/glb/keyShelf.glb",
+      "./assets/glb/leanFoot.glb",
+      "./assets/glb/pullupAdapter.glb",
+      "./assets/glb/remoteMount.glb",
+      "./assets/glb/role.glb",
+      "./assets/glb/shelf.glb",
+      "./assets/glb/showerMount.glb",
+      "./assets/glb/showerScrew.glb",
+      "./assets/glb/spongeHolder.glb",
+      // "./assets/glb/thirdHand.glb",
+    ];
+
+    const materials = [
+      new THREE.MeshStandardMaterial({
+        color: 0x7cb9e8,
+        roughness: 0.2,
+        metalness: 0.8,
+      }), // Light Blue
+      new THREE.MeshStandardMaterial({
+        color: 0xd2527f,
+        roughness: 0.4,
+        metalness: 0.6,
+      }), // Mauve
+      new THREE.MeshStandardMaterial({
+        color: 0x9b59b6,
+        roughness: 0.3,
+        metalness: 0.7,
+      }), // Purple
+      new THREE.MeshStandardMaterial({
+        color: 0x1abc9c,
+        roughness: 0.2,
+        metalness: 0.8,
+      }), // Teal
+      new THREE.MeshStandardMaterial({
+        color: 0xe74c3c,
+        roughness: 0.5,
+        metalness: 0.5,
+      }), // Red
+    ];
+
+    const draggableObjects = [];
+
+    const MainScene = () => {
+      const sceneContainer = document.getElementById("scene-container");
+      const scene = new THREE.Scene();
+      scene.background = new THREE.Color(0x151612);
+
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      camera.position.set(0, 35, 35);
+      camera.rotateX(-0.7);
+
+      // Renderer
+      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      sceneContainer.appendChild(renderer.domElement);
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+      // Lights
+      const shadowLight = new THREE.PointLight(0x00b381, 5, 100);
+      shadowLight.position.set(0, 50, -60);
+      shadowLight.castShadow = true;
+      shadowLight.shadow.mapSize.width = 1024;
+      shadowLight.shadow.mapSize.height = 1024;
+      shadowLight.shadow.camera.near = 0.5;
+      shadowLight.shadow.camera.far = 50;
+      const helperRight = new THREE.PointLightHelper(shadowLight, 1);
+      scene.add(helperRight);
+      scene.add(shadowLight);
+
+      scene.add(new THREE.AmbientLight(0xffffff, 1));
+
+      // initialize physics
+      const physics = new AmmoPhysics(scene);
+      // physics.debug.enable(true);
+
+      // Ground
+      const groundMesh = physics.add.ground({
+        width: 300,
+        height: 300,
+      });
+      const groundColor = 0x151612;
+      const groundMaterial = new THREE.MeshPhongMaterial({
+        color: groundColor,
+      });
+      groundMesh.material = groundMaterial;
+      groundMesh.receiveShadow = true;
+
+      modelPaths.forEach((modelPath, index) => {
+        // add .glb files as physics objects
+        loadModelWithPhysicsAndRandomMaterial(
+          modelPath,
+          new THREE.Vector3(
+            -50 + index * 6,
+            20,
+            -20 + THREE.MathUtils.randFloatSpread(-25)
+          ),
+          "convex",
+          index
+        );
+      });
+
+      const clock = new THREE.Clock();
+
+      const animate = () => {
+        physics.update(clock.getDelta() * 1000);
+        physics.updateDebugger();
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(animate);
+      };
+      requestAnimationFrame(animate);
+
+      // Functions
+      function loadModelWithPhysicsAndRandomMaterial(
+        url,
+        position,
+        shapeType,
+        index
+      ) {
+        new THREE.GLTFLoader().load(url, function (gltf) {
+          const object = gltf.scene.children[0];
+          object.position.copy(position);
+
+          // Assign a random material to each mesh in the object
+          object.traverse((child) => {
+            if (child.isMesh) {
+              const randomMaterial =
+                materials[Math.floor(Math.random() * materials.length)];
+              child.material = randomMaterial;
+              child.castShadow = true;
+              child.receiveShadow = true;
+            }
+          });
+          object.rotation.set(1 * index, -1 * index, index);
+
+          scene.add(object);
+          physics.add.existing(object, { shape: shapeType });
+
+          draggableObjects.push(object);
+        });
+      }
+    };
+    PhysicsLoader("/lib/PhysicsLoader", () => MainScene());
+  }
+  sceneInitialized = true;
+};
